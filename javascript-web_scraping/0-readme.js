88b164f6408cd8aea18a3726742632753f7f0c4d
amina <amina.1111@yahoo.fr>
2024-05-14 22:12:47 +0100

    script that reads and prints the content of a file.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const args = process.argv;
+
+if (args.length !== 3) {
+  console.error("Expected exactly one argument!");
+  process.exit(1);
+}
+else {
+ 
+// Use fs.readFile() method to read the file
+fs.readFile(args[2], 'utf8', function(err, data){
+  if (err) {
+    console.error(err);
+  }
+    console.log(data);
+});
+}
