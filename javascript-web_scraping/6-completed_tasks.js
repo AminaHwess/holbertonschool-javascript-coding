fbf5e40a81bbc6f291aeec5256d5225434504545
amina <amina.1111@yahoo.fr>
2024-05-15 02:00:02 +0100

    script that computes the number of tasks completed by user id.

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const args = process.argv;
+let j = 0;
+const userCountDict = {}; // Object to store user IDs and counts
+
+request(args[2], (error, response, body) => {
+  // Printing the error if occurred
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    // Assuming data is sorted by userId
+    let currentUser = data[0].userId;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].userId === currentUser) {
+        if (data[i].completed === true) {
+          j++;
+        }
+      } else {
+        userCountDict[currentUser] = j; // Assign count to user ID in dictionary
+        currentUser = data[i].userId;
+        j = data[i].completed === true ? 1 : 0;
+      }
+    }
+    // Assign the count for the last user
+    userCountDict[currentUser] = j;
+
+    // Print the dictionary
+    console.log(userCountDict);
+  }
+});
